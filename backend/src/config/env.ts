f30489b7f08ae1c07bf82a3708b95b774e33013c
iamflubin <f.lubin@outlook.fr>
2025-07-06 17:50:15 +0200

    build: fix build process

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,23 +1,19 @@
 import dotenv from 'dotenv';
 import { z } from 'zod';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
   PORT: z.string().default('5000'),
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
-  JWT_EXPIRES_IN_S: z
-    .string()
-    .default('900')
-    .transform(val => parseInt(val, 10)),
-  REFRESH_TOKEN_EXPIRES_IN_MS: z
-    .string()
-    .default('604800000')
-    .transform(val => parseInt(val, 10)),
+  JWT_EXPIRES_IN_S: z.string().transform(val => parseInt(val, 10)),
+  REFRESH_TOKEN_EXPIRES_IN_MS: z.string().transform(val => parseInt(val, 10)),
   REFRESH_TOKEN_COOKIE_NAME: z.string(),
-  CORS_ORIGINS: z.string().default('http://localhost:3000'),
+  CORS_ORIGINS: z.string(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
