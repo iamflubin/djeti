a5873a2aa399ac620bb37bd7ac3e1e528d8937a7
iamflubin <f.lubin@outlook.fr>
2025-07-13 19:06:39 +0200

    chore: remove metrics

--- backend/src/routes/metrics.route.ts
+++
@@ -1,9 +0,0 @@
-import { getSummary } from '@/controllers/metrics.controller';
-import { requireAuth } from '@/middlewares/require-auth';
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/summary', requireAuth, getSummary);
-
-export default router;
